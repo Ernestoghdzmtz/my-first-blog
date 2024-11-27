ca3978ecb328b04517d28beaae9d3431ba21b19d
Ernesto <ehernandezmarti@ivytech.edu>
2024-11-27 03:40:36 -0800

    Changed the HTML for the site.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
-from .models import Post  # Asegúrate de que el modelo 'Post' esté importado
+#from .models import Post  # Asegúrate de que el modelo 'Post' esté importado
 
 def post_list(request):
-    posts = Post.objects.all()  # Asumiendo que tienes un modelo llamado Post
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    #posts = Post.objects.all()  # Asumiendo que tienes un modelo llamado Post
+    #return render(request, 'blog/post_list.html', {'posts': posts})
+	return render(request, 'blog/post_list.html', {})
 # Create your views here.
